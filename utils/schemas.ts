a0ee595f916f6f594710f93f98550f93bc250b2f
Anvar <anvar.ziyodov@trescal.com>
2024-10-16 16:52:41 +0900

    Chore login and sign up components

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,8 +1,15 @@
-import * as  z from 'zod'
-import {ZodSchema} from 'zod'
+import * as z from "zod";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
-  firstName: z.string().min(3, {message: 'Your firstname should be at least 3 letters'}),
-  lastName: z.string().min(3, {message: 'Your lastname should be at least 3 letters'}),
-  userName: z.string().min(3, {message: 'Your username should be at least 3 letters'})
-})
+  firstName: z
+    .string()
+    .min(3, { message: "Your firstname should be at least 3 letters" }),
+  lastName: z
+    .string()
+    .min(3, { message: "Your lastname should be at least 3 letters" }),
+  userName: z
+    .string()
+    .min(3, { message: "Your username should be at least 3 letters" }),
+});
