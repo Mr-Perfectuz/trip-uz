cc9fa276d7a0548d6b9b405d2e8ca9af3e44eb08
Anvar <anvar.ziyodov@trescal.com>
2024-10-17 14:31:25 +0900

    Feat: develop rentals page

---
+++ app/rentals/create/page.tsx
@@ -0,0 +1,40 @@
+import FormInput from "@/components/form/FormInput";
+import FormContainer from "@/components/form/FormContainer";
+import { createPropertyAction } from "@/utils/actions";
+import { SubmitButton } from "@/components/form/Buttons";
+import PriceInput from "@/components/form/PriceInput";
+import CategoriesInput from "@/components/form/Categories";
+
+function CreatePropertyPage() {
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitilize">
+        Create property
+      </h1>
+      <div className="border p-8 rounded">
+        <h3 className="text-lg mb-4 font-medium">General Info</h3>
+        <FormContainer action={createPropertyAction}>
+          <div className="grid md:grid-cols-2 gap-8 mb-4">
+            <FormInput
+              name="name"
+              type="text"
+              label="Name (20 imit)"
+              defaultValue="Cabin in Latvia "
+            />
+            <FormInput
+              name="tagline"
+              type="text"
+              label="Name (30 imit)"
+              defaultValue="Dream Getaway awaits you here"
+            />
+            <PriceInput />
+            <CategoriesInput />
+          </div>
+          {/* text area */}
+          <SubmitButton text="create rental" className="mt-12" />
+        </FormContainer>
+      </div>
+    </section>
+  );
+}
+export default CreatePropertyPage;
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,17 +1,17 @@
-import * as z from 'zod';
+import * as z from "zod";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ZodSchema } from 'zod';
+import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
   firstName: z
     .string()
-    .min(3, { message: 'Your firstname should be at least 3 letters' }),
+    .min(3, { message: "Your firstname should be at least 3 letters" }),
   lastName: z
     .string()
-    .min(3, { message: 'Your lastname should be at least 3 letters' }),
+    .min(3, { message: "Your lastname should be at least 3 letters" }),
   userName: z
     .string()
-    .min(3, { message: 'Your username should be at least 3 letters' }),
+    .min(3, { message: "Your username should be at least 3 letters" }),
 });
 
 export function validatedWithZodSchema<T>(
@@ -22,7 +22,7 @@ export function validatedWithZodSchema<T>(
 
   if (!result.success) {
     const err = result.error.errors.map((err) => err.message);
-    throw new Error(err.join(','));
+    throw new Error(err.join(","));
   }
   return result.data;
 }
@@ -32,15 +32,61 @@ export const ImageSchema = z.object({
 
 function validateFile() {
   const maxUploadSize = 1024 * 1024;
-  const acceptedFilesTypes = ['image/'];
+  const acceptedFilesTypes = ["image/"];
   return z
     .instanceof(File)
     .refine((file) => {
       return !file || file.size <= maxUploadSize;
-    }, 'File size must be less than 1 MB')
+    }, "File size must be less than 1 MB")
     .refine((file) => {
       return (
         !file || acceptedFilesTypes.some((type) => file.type.startsWith(type))
       );
-    }, 'File must be an image !');
+    }, "File must be an image !");
 }
+
+export const propertySchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "name must be at least 2 characters.",
+    })
+    .max(100, {
+      message: "name must be less than 100 characters.",
+    }),
+  tagline: z
+    .string()
+    .min(2, {
+      message: "tagline must be at least 2 characters.",
+    })
+    .max(100, {
+      message: "tagline must be less than 100 characters.",
+    }),
+  price: z.coerce.number().int().min(0, {
+    message: "price must be a positive number.",
+  }),
+  category: z.string(),
+  description: z.string().refine(
+    (description) => {
+      const wordCount = description.split(" ").length;
+      return wordCount >= 10 && wordCount <= 1000;
+    },
+    {
+      message: "description must be between 10 and 1000 words.",
+    }
+  ),
+  country: z.string(),
+  guests: z.coerce.number().int().min(0, {
+    message: "guest amount must be a positive number.",
+  }),
+  bedrooms: z.coerce.number().int().min(0, {
+    message: "bedrooms amount must be a positive number.",
+  }),
+  beds: z.coerce.number().int().min(0, {
+    message: "beds amount must be a positive number.",
+  }),
+  baths: z.coerce.number().int().min(0, {
+    message: "bahts amount must be a positive number.",
+  }),
+  amenities: z.string(),
+});
