9aa5abf89dae0cb74306f7f0195ce5f959e8c4e0
Anvar <ziyodovanvar1999@gmail.com>
2024-10-17 21:27:11 +0900

    Feat: develop Favodite, PropertyList, EmptyList components

---
+++ components/card/FavouriteToggleButton.tsx
@@ -0,0 +1,5 @@
+function FavouriteToggleButton() {
+  return <div>FavouriteToggleButton</div>;
+}
+
+export default FavouriteToggleButton;
--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,5 +1,14 @@
-function PropertiesList() {
-  return <div>PropertiesList</div>;
+import { PropertyCardProps } from '@/utils/types';
+import PropertyCard from '../card/PropertyCard';
+
+function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
+  return (
+    <section>
+      {properties.map((property) => {
+        return <PropertyCard key={property.id} property={property} />;
+      })}
+    </section>
+  );
 }
 
 export default PropertiesList;
