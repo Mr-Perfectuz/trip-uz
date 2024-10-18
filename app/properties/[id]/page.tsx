0c8754c659c420898fb6a9d0cc35d23c72ebd475
Anvar <anvar.ziyodov@trescal.com>
2024-10-18 17:28:24 +0900

    Develop PropertyDetailsPage and BreadCrumbs to navigate the page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,5 @@
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 
@@ -6,8 +8,18 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   if (!property) redirect("/");
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
-  console.log(details);
-  return <div>PropertyDetailsPage</div>;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold cap">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          {/* share button */}
+          <FavoriteToggleButton propertyId={property.id}/>
+        </div>
+      </header>
+    </section>
+  );
 }
 
 export default PropertyDetailsPage;
