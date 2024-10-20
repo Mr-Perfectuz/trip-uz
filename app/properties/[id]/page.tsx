11d9528954f557625a88315672953b918f6010a8
Anvar <ziyodovanvar1999@gmail.com>
2024-10-20 16:12:30 +0900

    Feat develop ImageContainer page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,20 +1,22 @@
-import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
-import BreadCrumbs from "@/components/properties/BreadCrumbs";
-import ShareButton from "@/components/properties/ShareButton";
-import { fetchPropertyDetails } from "@/utils/actions";
-import { redirect } from "next/navigation";
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import PropertyRating from '@/components/card/PropertyRating';
+import BreadCrumbs from '@/components/properties/BreadCrumbs';
+import ImageContainer from '@/components/properties/ImageContainer';
+import ShareButton from '@/components/properties/ShareButton';
+import { fetchPropertyDetails } from '@/utils/actions';
+import { redirect } from 'next/navigation';
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
-  if (!property) redirect("/");
+  if (!property) redirect('/');
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
   return (
     <section>
       <BreadCrumbs name={property.name} />
-      <header className="flex justify-between items-center mt-4">
-        <h1 className="text-4xl font-bold capitalize">{property.tagline}</h1>
-        <div className="flex items-center gap-x-4">
+      <header className='flex justify-between items-center mt-4'>
+        <h1 className='text-4xl font-bold capitalize'>{property.tagline}</h1>
+        <div className='flex items-center gap-x-4'>
           {/* share button */}
           <ShareButton
             propertyId={property.id}
@@ -23,6 +25,18 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12'>
+        <div className='lg:col-span-8'>
+          <div className='flex gap-x-4 items-center'>
+            <h1 className='text-xl font-bold'>{property.name}</h1>
+            <PropertyRating propertyId={property.id} inPage />
+          </div>
+        </div>
+        <div className='lg:col-span-4 flex flex-col items-center'>
+          {/* calendar */}
+        </div>
+      </section>
     </section>
   );
 }
