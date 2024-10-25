495dfe37ca8a320c5f65bd354dbf97e01fc1885c
Anvar <anvar.ziyodov@trescal.com>
2024-10-25 10:24:21 +0900

    Feat: develop Charts, ChartsContainer, Stats, ChartsContainer, StatsCard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,22 @@
+import { Suspense } from "react";
+import StatsContainer from "@/components/admin/StatsContainer";
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContaiener,
+} from "@/components/admin/Loading";
+import ChartsContainer from "@/components/admin/ChartsContainer";
+
 function AdminPage() {
-  return <div>AdminPage</div>;
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContaiener />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  );
 }
 
 export default AdminPage;
