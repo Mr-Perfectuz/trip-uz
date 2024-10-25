9e292cb917f9cca8cb20c8c612525abc0a2d21d0
Anvar <ziyodovanvar1999@gmail.com>
2024-10-25 23:18:04 +0900

    Feat: Fix errors

--- utils/superbase.ts
+++ utils/superbase.ts
@@ -4,14 +4,14 @@ const bucket = 'trip-uz';
 const url = process.env.SUPERBASE_URL as string;
 const key = process.env.SUPERBASE_KEY as string;
 
-const superbse = createClient(url, key);
+const superbase = createClient(url, key);
 export const uploadImage = async (image: File) => {
   const timestamp = Date.now();
   const newName = `${timestamp}-${image.name}`;
 
-  const { data } = await superbse.storage
+  const { data } = await superbase.storage
     .from(bucket)
     .upload(newName, image, { cacheControl: '3600' });
-  if (!data) throw new Error('Image upload if failed !');
-  return superbse.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
+  if (!data) throw new Error('Image upload failed !');
+  return superbase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
 };
